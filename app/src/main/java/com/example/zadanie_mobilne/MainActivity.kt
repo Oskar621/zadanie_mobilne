a87b6403ca4834d24cc5a5aedd68f4c427d7f6f5
OGMaksiu <mateckimaksymilian@gmail.com>
2022-10-21 14:50:38 +0200

    zrobione onclicki do dopisz i przywroc zadanie skonczone

--- app/src/main/java/com/example/zadanie_mobilne/MainActivity.kt
+++ app/src/main/java/com/example/zadanie_mobilne/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.zadanie_mobilne
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val dopisz = findViewById<Button>(R.id.dopisz)
+        val przywroc = findViewById<Button>(R.id.przywroc)
+        val tekst = findViewById<TextView>(R.id.imie_nazwisko)
+        val wpisz = findViewById<EditText>(R.id.wpisz)
+
+        dopisz.setOnClickListener {
+            tekst.text = tekst.text.toString() + wpisz.text
+        }
+        przywroc.setOnClickListener {
+            tekst.text = "Oskar Świderski"
+        }
     }
 }
